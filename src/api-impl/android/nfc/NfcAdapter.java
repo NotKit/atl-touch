b1d904befdd4d305e30af5e6f2dde6cc05987142
Val Packett <val@packett.cool>
2026-03-22 22:42:00 -0300

    Add more android.nfc getDefaultAdapter variants

--- src/api-impl/android/nfc/NfcAdapter.java
+++ src/api-impl/android/nfc/NfcAdapter.java
@@ -4,6 +4,11 @@ import android.content.Context;
 
 public class NfcAdapter {
 
+	//@UnsupportedAppUsage
+	public static NfcAdapter getDefaultAdapter() {
+		return null;
+	}
+
 	public static NfcAdapter getDefaultAdapter(Context context) {
 		return null;
 	}
