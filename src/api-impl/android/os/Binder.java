9daeef399c53345a21908e74fa4aa1fb994ec0ad
TheKit <thekit@disroot.org>
2026-08-11 08:15:54 +0300

    api-impl: give Binder onTransact and IBinder its constants
    Every aidl-generated Stub overrides onTransact and switches on
    FIRST_CALL_TRANSACTION + n; neither existed. There is no binder driver,
    so transact() calls onTransact() in process.

--- src/api-impl/android/os/Binder.java
+++ src/api-impl/android/os/Binder.java
@@ -2,9 +2,23 @@ package android.os;
 
 import android.content.Context;
 
+import java.io.FileDescriptor;
+
 public class Binder implements IBinder {
 
-	public void attachInterface(IInterface owner, String descriptor) {}
+	private IInterface owner;
+	private String descriptor;
+
+	public Binder() {}
+
+	public Binder(String descriptor) {
+		this.descriptor = descriptor;
+	}
+
+	public void attachInterface(IInterface owner, String descriptor) {
+		this.owner = owner;
+		this.descriptor = descriptor;
+	}
 
 	public static void flushPendingCommands() {}
 
@@ -13,10 +27,43 @@ public class Binder implements IBinder {
 	public static void restoreCallingIdentity(long identityToken) {}
 
 	@Override
-	public IInterface queryLocalInterface(String descriptor) { return null; }
+	public String getInterfaceDescriptor() { return descriptor; }
 
 	@Override
-	public boolean transact(int code, Parcel data, Parcel reply, int flags) { return false; }
+	public boolean pingBinder() { return true; }
+
+	@Override
+	public boolean isBinderAlive() { return true; }
+
+	@Override
+	public IInterface queryLocalInterface(String descriptor) {
+		return this.descriptor != null && this.descriptor.equals(descriptor) ? owner : null;
+	}
+
+	@Override
+	public void dump(FileDescriptor fd, String[] args) {}
+
+	@Override
+	public void dumpAsync(FileDescriptor fd, String[] args) {}
+
+	/**
+	 * There is no binder driver under ATL, so a transaction is delivered in
+	 * process: transact() calls straight through to onTransact().
+	 */
+	@Override
+	public boolean transact(int code, Parcel data, Parcel reply, int flags) throws RemoteException {
+		return onTransact(code, data, reply, flags);
+	}
+
+	protected boolean onTransact(int code, Parcel data, Parcel reply, int flags) throws RemoteException {
+		return false;
+	}
+
+	@Override
+	public void linkToDeath(DeathRecipient recipient, int flags) {}
+
+	@Override
+	public boolean unlinkToDeath(DeathRecipient recipient, int flags) { return true; }
 
 	public static int getCallingUid() { return Context.this_application.getApplicationInfo().uid; }
 
--- src/api-impl/android/os/IBinder.java
+++ src/api-impl/android/os/IBinder.java
@@ -1,10 +1,38 @@
 package android.os;
 
+import java.io.FileDescriptor;
+
 public interface IBinder {
 
-	public interface DeathRecipient {}
+	public static final int FIRST_CALL_TRANSACTION = 0x00000001;
+	public static final int LAST_CALL_TRANSACTION = 0x00ffffff;
+	public static final int PING_TRANSACTION = ('_' << 24) | ('P' << 16) | ('N' << 8) | 'G';
+	public static final int DUMP_TRANSACTION = ('_' << 24) | ('D' << 16) | ('M' << 8) | 'P';
+	public static final int SHELL_COMMAND_TRANSACTION = ('_' << 24) | ('C' << 16) | ('M' << 8) | 'D';
+	public static final int INTERFACE_TRANSACTION = ('_' << 24) | ('N' << 16) | ('T' << 8) | 'F';
+	public static final int TWEET_TRANSACTION = ('_' << 24) | ('T' << 16) | ('W' << 8) | 'T';
+	public static final int LIKE_TRANSACTION = ('_' << 24) | ('L' << 16) | ('I' << 8) | 'K';
+	public static final int FLAG_ONEWAY = 0x00000001;
+
+	public interface DeathRecipient {
+		public void binderDied();
+	}
+
+	public String getInterfaceDescriptor() throws RemoteException;
+
+	public boolean pingBinder();
+
+	public boolean isBinderAlive();
 
 	public IInterface queryLocalInterface(String descriptor);
 
-	public boolean transact(int code, Parcel data, Parcel reply, int flags);
+	public void dump(FileDescriptor fd, String[] args) throws RemoteException;
+
+	public void dumpAsync(FileDescriptor fd, String[] args) throws RemoteException;
+
+	public boolean transact(int code, Parcel data, Parcel reply, int flags) throws RemoteException;
+
+	public void linkToDeath(DeathRecipient recipient, int flags) throws RemoteException;
+
+	public boolean unlinkToDeath(DeathRecipient recipient, int flags);
 }
